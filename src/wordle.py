325da8ebd76ad9243e967e3e3383762ef9f34839
Jason Wang <jason.chengyao.wang@gmail.com>
2022-02-24 23:34:27 +0800

    implemented wordle match function with tests

--- src/wordle.py
+++ src/wordle.py
@@ -1,3 +1,4 @@
+from collections import Counter
 
 def match_guess_to_answer(answer: str, guess: str, lower: bool = False) -> str:
     """Match the guess to answer and returns the result.
@@ -7,9 +8,7 @@ def match_guess_to_answer(answer: str, guess: str, lower: bool = False) -> str:
     `P` means the letter is `present` in the answer but in the wrong position,
     and `A` means the letter is `absent` in the answer.
 
-    For example,
-
-    Throws error when length of [`guess`] is not equal to [`answer`].
+    [`guess`] and [`answer`] must be the same length.
 
     Args:
         answer (str): The answer
@@ -18,52 +17,26 @@ def match_guess_to_answer(answer: str, guess: str, lower: bool = False) -> str:
     Returns:
         str: The result
     """
+    assert len(answer) == len(guess), "guess and answer must be the same length"
 
-    # TODO: error when len of guess less than answer
-    assert(len(answer) == len(guess))
+    answer = answer.lower() if lower else answer.upper()
+    guess = guess.lower() if lower else guess.upper()
 
-    answer = answer.upper() if not lower else answer.lower()
-    guess = guess.upper() if not lower else answer.lower()
+    # A count of number of present letters
+    present = Counter(answer) & Counter(guess)
 
     ret = ['A' for _ in range(len(answer))]
 
-    # match letters in correct position
-    for i in range(len(answer)):
-        if answer[i] == guess[i]:
+    # Check for correct letters
+    for i, c in enumerate(guess):
+        if c == answer[i]:
             ret[i] = 'C'
-    
-    # match letters present in answer but wrong position
-    for i in range(len(answer)):
-        # if ret is not correct, check if it is in the answer
-        if ret[i] != 'C':
-            # list of indexes present in answer
-            for i0 in range(len(answer)):
-                if answer[i0] == guess[i]:
-                    if ret[i0] != 'C':
-                        ret[i] = 'P'
-    return ''.join(ret)
+            present[c] -= 1
 
-class DefinitelyNotWordle():
-    """A mock wordle game
-    """
-    
-    def __init__(self, answer: str) -> None:
-        self.answer = answer.upper()
+    # Check for present letters
+    for i, c in enumerate(guess):
+        if present[c] > 0:
+            ret[i] = 'P'
+            present[c] -= 1
 
-    def guess(self, guess: str) -> str:
-        """Returns the result to a guess.
-
-        Args:
-            guess (str): 
-
-        Returns:
-            str: A string in the format of: C = Correct, P = Present, A = Absent
-        """
-        return match_guess_to_answer(self.answer, guess)
-    
-if __name__ == '__main__':
-    game = DefinitelyNotWordle(answer='JASON')
-
-    # answer='JASON', guess='JOHNN', ret='CPAAC'
-    res = game.guess('JOHNN')
-    print(f'{res} { "==" if res == "CPAAC" else "!=" } \'CPAAC\'')
+    return ''.join(ret).lower() if lower else ''.join(ret).upper()
--- tests/test_wordle.py
+++ tests/test_wordle.py
@@ -1,45 +1,44 @@
 import unittest
-import src.wordle as wordle
+from src.wordle import match_guess_to_answer as match
 
-class TestStringMethods(unittest.TestCase):
+class TestWordleMatch(unittest.TestCase):
 
-    def test_answer_are_auto_capitalized(self):
-        game = wordle.DefinitelyNotWordle(answer='jason')
-        self.assertEqual(game.answer, 'JASON')
-
-    def test_guess(self):
-        game = wordle.DefinitelyNotWordle(answer='JASON')
-        self.assertEqual(game.guess('JOHNN'), 'CPAAC')
+    def test_answer_and_guess_same_length(self):
+        with self.assertRaises(AssertionError) as e:
+            match('JASON', 'A')
+        self.assertEqual(e.exception.args, ('guess and answer must be the same length',))
 
     def test_guess_is_all_correct(self):
-        game = wordle.DefinitelyNotWordle(answer='JASON')
-        self.assertEqual(game.guess('JASON'), 'CCCCC')
+        self.assertEqual(match('ABCDE', 'ABCDE'),'CCCCC')
+        self.assertEqual(match('AAAAA', 'AAAAA'),'CCCCC')
 
-    def test_guess_is_all_correct_except_one(self):
-        game = wordle.DefinitelyNotWordle(answer='JASON')
-        self.assertEqual(game.guess('AASON'), 'ACCCC')
+    def test_guess_is_all_absent(self):
+        self.assertEqual(match('ABCDE', 'FGHIJ'),'AAAAA')
+        self.assertEqual(match('AAAAA', 'BBBBB'),'AAAAA')
 
-    def test_guess_is_all_incorrect(self):
-        game = wordle.DefinitelyNotWordle(answer='JASON')
-        self.assertEqual(game.guess('THREE'), 'AAAAA')
-
-    def test_wordle_246(self):
-        game = wordle.DefinitelyNotWordle(answer='tacit')
-        self.assertEqual(game.guess('horse'), 'AAAAA')
-        self.assertEqual(game.guess('plans'), 'AAPAA')
-        self.assertEqual(game.guess('crane'), 'PAPAA')
-        self.assertEqual(game.guess('soare'), 'AAPAA')
-        self.assertEqual(game.guess('three'), 'CAAAA')
-        self.assertEqual(game.guess('trace'), 'CAPPA')
-
-    def test_wordle_246_fn(self):
-        self.assertEqual(wordle.match_guess_to_answer('tacit', 'horse'), 'AAAAA')
-        self.assertEqual(wordle.match_guess_to_answer('tacit', 'plans'), 'AAPAA')
-        self.assertEqual(wordle.match_guess_to_answer('tacit', 'crane'), 'PAPAA')
-        self.assertEqual(wordle.match_guess_to_answer('tacit', 'soare'), 'AAPAA')
-        self.assertEqual(wordle.match_guess_to_answer('tacit', 'three'), 'CAAAA')
-        self.assertEqual(wordle.match_guess_to_answer('tacit', 'trace'), 'CAPPA')
+    def test_guess_have_present_letters(self):
+        self.assertEqual(match('BABBB', 'AAAAA'), 'ACAAA')
+        self.assertEqual(match('BABAB', 'AAAAA'), 'ACACA')
+        self.assertEqual(match('BABBB', 'ABAAA'), 'PPAAA')
         
+        self.assertEqual(match('JASON', 'ABAAA'), 'PAAAA')
+        self.assertEqual(match('JASON', 'AABBB'), 'ACAAA')
+        self.assertEqual(match('ABCDD', 'DDDDD'), 'AAACC')
+        self.assertEqual(match('ABCDD', 'DDDAA'), 'PPAPA')
+    
+    ### Using result from actual wordle to test
+    def test_wordle_246(self):
+        self.assertEqual(match('tacit', 'horse'), 'AAAAA')
+        self.assertEqual(match('tacit', 'plans'), 'AAPAA')
+        self.assertEqual(match('tacit', 'crane'), 'PAPAA')
+        self.assertEqual(match('tacit', 'soare'), 'AAPAA')
+        self.assertEqual(match('tacit', 'three'), 'CAAAA')
+        self.assertEqual(match('tacit', 'trace'), 'CAPPA')
+        self.assertEqual(match('tacit', 'tacit'), 'CCCCC')
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_wordle_250(self):
+        self.assertEqual(match('bloke', 'tacit'), 'AAAAA')
+        self.assertEqual(match('bloke', 'doner'), 'APAPA')
+        self.assertEqual(match('bloke', 'bowie'), 'CPAAC')
+        self.assertEqual(match('bloke', 'bloke'), 'CCCCC')
+        
