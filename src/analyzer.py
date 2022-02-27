81e5b34d155cdf8e1b999c39a099b13eb3cbb234
Jason Wang <jason.chengyao.wang@gmail.com>
2022-02-27 23:04:52 +0800

    added word ranking to analyzer

--- src/analyzer.py
+++ src/analyzer.py
@@ -3,11 +3,36 @@ import string
 from typing import List, Counter
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 class Analyzer:
     def __init__(self, words: np.ndarray) -> None:
         self.words = words
+
+    def get_ranked_words(self):
+        ret = pd.DataFrame(
+            data = {
+                'rank': self.get_ranking_with_naive_method()
+            },
+            index = [
+                ''.join([chr(c+ord('A')) for c in w])
+                for w in self.words
+            ],
+        )
+        ret.sort_values('rank', axis='index', ascending=False, inplace=True)
+        return ret
+
+    def get_ranking_with_naive_method(self):
+        """Naive method to analyze words.
+
+        Using only the frequency of letters in the whole word list to generate the ranking of each words
+        """
+        freq = self.letter_freq_in_all()
+        return [ 
+            sum( freq[c] for c in set(w) ) # set so the ranking is weighted less to words with duplicated letters
+            for w in self.words
+        ]
 
     def letter_count_in_pos(self) -> List[Counter]:
         """finds the number of appearance of letters in each position
