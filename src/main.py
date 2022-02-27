d2ac752fa3418117536f10fdc2739b703a59a526
Jason Wang <jason.chengyao.wang@gmail.com>
2022-02-27 23:05:38 +0800

    implemented solver with naive method

--- src/main.py
+++ src/main.py
@@ -1,37 +1,43 @@
+import json
 from pathlib import Path
-from typing import Optional
+from typing import Optional, List
+from multiprocessing import Pool
+import os
 
 from wordle import match_guess_to_answer
 from solver import Solver
 
-def read_word_list(path: Path, sep: str = '\n'):
+def read_words_list(path: Path, sep: str = '\n'):
     with open(path) as f:
         return f.read().split(sep)
 
-def get_scrabble_5_letter_words():
-    p = Path('files', 'scrabble_5_letter_words.txt')
-    return read_word_list(p)
-
-def get_words_list(path: Optional[Path] = None, sep: str = '\n'):
-    return read_word_list(path, sep) if path else get_scrabble_5_letter_words()
-
-def solve_wordle(answer: str) -> int:
-    # Initialize solver
-    solver = Solver(word_list=get_scrabble_5_letter_words())
-
-    # Guess
-    hint = ''
-    for i in range(0, 6):
-        if i == 0:
-            guess = solver.first_guess()
-        else:
-            guess = solver.subsequent_guess(hint)
-        hint = match_guess_to_answer(answer, guess)
-        if hint == 'CCCCC':
-            return i
-    return -1
-
+def compute_effectiveness(answers: List[str], test_set: List[str], ranking_algorithm: str):
+    solver = Solver(word_list=test_set)    
+    with Pool(os.cpu_count()) as p:
+        ret = p.map(solver.solve, answers)
+    
+    print(ret)
+    with open('output.txt', 'w+') as file:
+        txt = '\n'.join([json.dumps(r) for r in ret])    
+        file.write(txt)
 
 if __name__ == '__main__':
-    # print(get_words_list()[:10])
-    solve_wordle('JASON')
+    answers = read_words_list(Path('files', 'wordle_list_of_answers.txt'))
+    test_set = read_words_list(Path('files', 'scrabble_5_letter_words.txt'))
+
+    compute_effectiveness(
+        answers = answers[:10],
+        test_set = test_set,
+        ranking_algorithm = 'naive' #
+    )
+
+    # import numpy as np
+    # with open('output.txt') as f:
+    #     txt = f.read()
+
+    # result = np.array([ line.split('=') for line in txt.split('\n') ])
+
+    # nums = np.array([ int(n) for n in result[:, 1] ])
+    # print(nums)
+    # print(f'Minimum guesses: {nums.min()}, Maximum guesses: {nums.max()}, Average guesses: {nums.mean()}')
+
--- src/solver.py
+++ src/solver.py
@@ -1,38 +1,160 @@
-from typing import List
+from random import randint
+from typing import Iterable, List
 
 import numpy as np
+from numpy.typing import NDArray
 
-from analyzer import Analyzer, Plotter
+from analyzer import Analyzer
+from wordle import match_guess_to_answer as match
+
+def match_correct(word: str, correct: Iterable[str]):
+    for w, c in zip(word, correct):
+        if w != c and c != '':
+            return False
+    return True
+
+def match_incorrect(word: str, incorrects: Iterable[Iterable[str]]):
+    for incorrect in incorrects:
+        for w, i in zip(word, incorrect):
+            if w == i and i != '':
+                return False
+    return True
+
+def filter_word_list(
+    word_array: NDArray, 
+    correct: Iterable[str],
+    incorrect: Iterable[Iterable[str]],
+    present: Iterable[str],
+    absent: Iterable[str]
+) -> NDArray:
+    """filter the word list based on 'Present' letters and 'Absent' letters
+
+    Args:
+        word_list (List[str]): list of words
+        present (Iterable[str]): list of letters present in the answer
+        absent (Iterable[str]): list of letters absent from the answer
+
+    Returns:
+        List[str]: filtered word list
+    """
+    return np.array([
+        w
+        for w in word_array
+        if set(w).issuperset(present) # True if every element in 'present' is in the word
+        and set(w).isdisjoint(absent) # True if every element in 'absent is NOT in the word
+        and match_correct(w, correct)
+        and match_incorrect(w, incorrect)
+    ])
 
 class Solver:
-    def __init__(self, word_list: List[str]):
+    def __init__(self, word_list: List[str], isupper: bool = True):
         self.word_list = word_list
-        self.analyzer = Analyzer(
-            np.array([list(word) for word in self.word_list])
-        )
+        self.isupper = isupper
 
-    def first_guess(self) -> str:
-        """Picks the optimal first guess
-        """
-        ...
+        # Initializations
+        self.word_array = np.array(self.word_list)
+        self.filter = self._initialize_filter()
 
-    def subsequent_guess(self, prev_hint: str) -> str:
+    """
+        Public Methods
+    """
+    def guess(self, prev_guess: str, hint: str) -> str:
         """Picks optimal guess with hints from previous guesses
-        """
-        ...
 
-    """Private Methods"""
-    def _analyze_words(self):
-        ...
+            Initial previous guess and hint are empty strings
+        """
+        if prev_guess == '' and hint == '':
+            word_array = self.word_array
+        else:
+            word_array = self._filtered_word_list(prev_guess, hint)
+
+        # Get new rankings
+        ranking = Analyzer(words=self._get_ord_array(word_array)).get_ranked_words()
+
+        # FIXME: this is so dumb
+        if ranking.iloc[0].name == prev_guess:
+            return ranking.iloc[ randint(1, len(ranking))-1 ].name 
+        return ranking.iloc[0].name
+
+    def solve(self, answer: str):
+        """
+        """
+        self.reset()
+        guesses, hints, rounds = [''], [''], 1
+        correct = 'C' * len(answer)
+        while hints[-1] != correct:
+            try:
+                guesses.append(self.guess(prev_guess=guesses[-1], hint=hints[-1]))
+            except Exception: # TODO: Better error handling
+                return {
+                    'answer': answer,
+                    'guesses': guesses[1:],
+                    'hints': hints[1:],
+                    'rounds': -1
+                }
+            hints.append(match(answer, guesses[-1]))
+            rounds += 1
+        #     print(f'Answer: {answer}, Guess: {guesses}, Hint: {hints}')
+        # print(f'Number of guesses for \'{answer}\': {rounds}')
+        return {
+            'answer': answer,
+            'guesses': guesses[1:],
+            'hints': hints[1:],
+            'rounds': rounds
+        }
+
+    def reset(self):
+        """Resets solver to initial state
+        """
+        self.filter = self._initialize_filter()
+    
+    """
+        Private Methods
+    """
+    def _initialize_filter(self):
+        return {
+            'present': [],
+            'absent': [],
+            'correct': ['' for _ in self.word_array[0]],
+            'incorrect': []
+        }
+
+    def _get_ord_array(self, word_array: np.ndarray):
+        diff = ord('A') if self.isupper else ord('a')
+        return np.array([ [ ord(c)-diff for c in word ] for word in word_array])
+
+    def _update_filter(self, prev_guess, hint):
+        # TODO: check if this can be simplified
+        self.filter['incorrect'].append(['' for _ in self.word_array[0]])
+        for i, (g, c) in enumerate(zip(prev_guess, hint)):
+            if (
+                g not in self.filter['correct'] 
+                and g not in self.filter['present'] 
+                and c == 'A' or c == 'a'
+            ):
+                self.filter['absent'].append(g)
+            elif c == 'P' or c == 'p':
+                self.filter['present'].append(g)
+                self.filter['incorrect'][-1][i] = g
+            elif c == 'C' or c == 'c':
+                self.filter['correct'][i] = g
+                if g in self.filter['absent']:
+                    self.filter['absent'].remove(g)
+            else:
+                ...
+    
+    def _filtered_word_list(self, prev_guess, hint) -> NDArray:
+        """filter the word list based on 'Present' letters and 'Absent' letters
+        """
+        self._update_filter(prev_guess, hint)
+        return np.array([
+            w
+            for w in self.word_array
+            if set(w).issuperset(self.filter['present']) # True if every element in 'present' is in the word
+            and set(w).isdisjoint(self.filter['absent']) # True if every element in 'absent is NOT in the word
+            and match_correct(w, self.filter['correct'])
+            and match_incorrect(w, self.filter['incorrect'])
+        ])
 
 if __name__ == '__main__':
-    import main
-    s = Solver(word_list=main.get_scrabble_5_letter_words())
-    print(f'Number of words: {len(s.word_list)}')
-
-    a = s.analyzer
-
-    p = Plotter()
-    p.save_frequency_analysis_plot(analyzer=a, filename='./out/frequency_analysis.jpg')
-    p.save_freq_bar_plot(analyzer=a, filename='./out/frequency.jpg')
-    
+    pass
